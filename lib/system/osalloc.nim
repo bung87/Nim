442819bcf008412bb6022345ddcca163dda8371e
unknown <crc32@qq.com>
2022-11-13 22:44:29 +0800

    temp

--- lib/system/osalloc.nim
+++ lib/system/osalloc.nim
@@ -193,7 +193,7 @@ elif defined(windows) and not defined(StandaloneHeapSize):
     #VirtualFree(p, size, MEM_DECOMMIT)
 
 elif hostOS == "standalone" or defined(StandaloneHeapSize):
-  const StandaloneHeapSize {.intdefine.}: int = 1024 * PageSize
+  const StandaloneHeapSize {.intdefine.}: int = 64 * sizeof(int) * PageSize - 1
   var
     theHeap: array[StandaloneHeapSize div sizeof(float64), float64] # 'float64' for alignment
     bumpPointer = cast[int](addr theHeap)
