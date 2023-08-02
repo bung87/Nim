93a55ff579ed56715a313ec1904938ddfe8da266
bung87 <crc32@qq.com>
2023-08-02 20:39:15 +0800

    add test case

---
+++ tests/t22284.nim
@@ -0,0 +1,21 @@
+import std/macros
+
+macro h(): untyped =
+  result = newTree(nnkStmtList)
+  result.add quote do:
+    new int
+
+type F = object
+
+proc j[SecondarySources](
+    uRef: ref SecondarySources,
+    u: proc (config: F, sources: ref SecondarySources)): F =
+  u(result, uRef)
+
+template programMain(body: untyped) =
+  proc main {.raises: [].} = body  # doesn't SIGSEGV without this {.raises: [].}
+  main()
+
+programMain:
+  var uRef = h()
+  discard j(uRef, u = (proc(config: F, sources: auto) {.raises: [].} = discard))
